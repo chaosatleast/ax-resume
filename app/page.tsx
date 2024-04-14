cd11bdedbaaf972737dc7f3ddbc8275147116066
alice990917 <alice990917@gmail.com>
2024-04-14 11:40:50 +0000

    base

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+
+    <div className="h-16 w-16 bg-gradient-to-r from-purple-600 to-blue-600 hover:from-blue-600 hover:to-purple-600 hover:ease-in-out hover:duration-500">
+
+    </div>
+    </main>
+  );
+}
